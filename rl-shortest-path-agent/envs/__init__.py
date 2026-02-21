9226068ee7af54867dea60463cff4d884ac62204
George Tebelev <tebelev99@mail.ru>
2026-02-21 22:26:25 +0300

    feat: add ShortestPathDataset class and dataset creation functionality

--- rl-shortest-path-agent/envs/__init__.py
+++ rl-shortest-path-agent/envs/__init__.py
@@ -1,3 +1,3 @@
-from envs.shortest_path import PathEnv, PathVerifier
+from envs.shortest_path import PathEnv, PathVerifier, create_benchmark_datasets, ShortestPathDataset
 
 # from envs.dataset import ShortestPathDataset
---
+++ rl-shortest-path-agent/scripts/dataset.py
@@ -0,0 +1,4 @@
+from envs import create_benchmark_datasets
+
+if __name__ == "__main__":
+    create_benchmark_datasets()
