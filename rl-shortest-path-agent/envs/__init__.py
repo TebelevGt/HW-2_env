80fdf06ece5c24b1f0ab6c9168a9940ee980ea9e
George Tebelev <tebelev99@mail.ru>
2026-02-21 22:15:21 +0300

    fix: comment out import for ShortestPathDataset in __init__.py

--- rl-shortest-path-agent/envs/__init__.py
+++ rl-shortest-path-agent/envs/__init__.py
@@ -1,2 +1,4 @@
 from envs.shortest_path import PathEnv, PathVerifier
-from envs.dataset import ShortestPathDataset
+
+# from envs.dataset import ShortestPathDataset
+from .prompts import generate_shortest_path_prompt
