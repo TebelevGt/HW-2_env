c573275cdb6306c9b647b577e62ee5fc72fffa01
George Tebelev <tebelev99@mail.ru>
2026-02-21 22:33:27 +0300

    fix: update import statement for ShortestPathDataset in __init__.py

--- rl-shortest-path-agent/envs/__init__.py
+++ rl-shortest-path-agent/envs/__init__.py
@@ -1,1 +1,1 @@
-from envs.shortest_path import PathEnv, PathVerifier, create_benchmark_datasets, ShortestPathDataset
+from .shortest_path import PathEnv, PathVerifier, ShortestPathDataset, create_benchmark_datasets
