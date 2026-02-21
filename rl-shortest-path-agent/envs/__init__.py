999a5ea045860fb287547347c57be864450a367a
George Tebelev <tebelev99@mail.ru>
2026-02-21 22:28:54 +0300

    fix: remove ShortestPathDataset import from __init__.py

--- rl-shortest-path-agent/envs/__init__.py
+++ rl-shortest-path-agent/envs/__init__.py
@@ -1,3 +1,1 @@
-from envs.shortest_path import PathEnv, PathVerifier, create_benchmark_datasets, ShortestPathDataset
-
-# from envs.dataset import ShortestPathDataset
+from envs.shortest_path import PathEnv, PathVerifier, create_benchmark_datasets
